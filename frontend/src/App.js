cdb4ae590787c492f393762f7067c78e4f58ab62
Khalid Bouychou <khbouych@e3r6p1.1337.ma>
2025-02-04 22:20:39 +0100

    setup front + .env file

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+
+import './App.css';
+import React from 'react';
+function App() {
+  return (
+    <h1 class="text-3xl font-bold underline decoration-white">
+    Hello world!
+  </h1>
+  );
+}
+
+export default App;
